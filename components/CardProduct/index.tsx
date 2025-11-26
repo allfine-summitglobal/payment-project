47b254f8685ae897ddd45ddaa357b54b52a23e7f
allfine-summitglobal <allfine@summitglobal.id>
2025-11-26 10:33:04 +0700

    update card for mobile

--- components/CardProduct/index.tsx
+++ components/CardProduct/index.tsx
@@ -1,9 +1,5 @@
-"use client"
-
 import { Card } from 'antd'
 import Image from 'next/image'
-import {_} from 'lodash'
-import {useResponsiveTruncate} from "@/app/hooks/truncate";
 import Link from "next/link";
 
 interface Product {
@@ -19,16 +15,17 @@ interface CardProductProps {
 }
 
 export default function CardProduct({ product }: CardProductProps) {
+  console.log('product', product)
   return (
     <Link href={`/${product.slug}`}>
       <Card
         hoverable
         variant={'outlined'}
-        className="rounded-2xl overflow-hidden bg-white shadow-md border border-gray-100 transition-all duration-300 hover:-translate-y-2 hover:shadow-xl"
+        className="rounded-2xl overflow-hidden bg-white shadow-md border border-gray-100 transition-all duration-300 hover:-translate-y-2 hover:shadow-xl "
         styles={{ body: { padding: 0 } }}
       >
         {/* Image Container */}
-        <div className="relative w-full aspect-[3/4] overflow-hidden bg-gray-100">
+        <div className="relative w-full sm:aspect-[3/4] overflow-hidden bg-gray-100">
           {/* Main Image */}
           <Image
             src={product.logoUrl}
@@ -42,8 +39,8 @@ export default function CardProduct({ product }: CardProductProps) {
           <div className="absolute bottom-0 left-0 right-0 h-20 bg-gradient-to-t from-black/60 to-transparent z-[3]" />
         </div>
 
-        {/* Info Section */}
-        <div className="bg-white p-4 text-center">
+        {/* Info Section - Desktop */}
+        <div className="hidden sm:block bg-white p-4 text-center">
           <div className="line-clamp-1 text-black text-base font-bold mb-1 tracking-wide">
             {product.title}
           </div>
@@ -51,7 +48,14 @@ export default function CardProduct({ product }: CardProductProps) {
             {product.subTitle}
           </div>
         </div>
+
       </Card>
+      {/* Info Section - Mobile (Di bawah Card) */}
+      <div className="sm:hidden p-2 text-center">
+        <div className="line-clamp-1 text-black text-sm font-bold mb-0.5 tracking-wide">
+          {product.title}
+        </div>
+      </div>
     </Link>
   )
 }
